d67b7b9352131f3550bc2832930f4a49156f1a2e
mangeshmandlik1 <49262061+mangeshmandlik1@users.noreply.github.com>
2019-04-05 11:25:54 +0530

    Add files via upload

---
+++ src/main/java/com/thoughtworks/movierental/TextStatement.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.movierental;
+
+public class TextStatement {
+
+	public String display(Rentals rentals, String name) {
+		String result = "Rental Record for " + name + "\n";
+		for (Rental each : rentals) {
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.amount()) + "\n";
+		}
+
+		// add footer lines result
+		result += "Amount owed is " + String.valueOf(rentals.totalamount()) + "\n";
+		result += "You earned " + String.valueOf(rentals.totalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+}
