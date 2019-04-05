3c3549724ed6c564d4571b38939cb283fa7efdad
mangeshmandlik1 <49262061+mangeshmandlik1@users.noreply.github.com>
2019-04-05 11:24:43 +0530

    Add files via upload

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -1,9 +1,59 @@
 package com.thoughtworks.movierental;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void test(){
+    	Customer customer = new Customer("Mangesh");
+//    	System.out.println(customer.statement());
+    }
 
+    @Test
+    public void testStatement(){
+    	Movie movie = new Movie("ABCD", 2);
+    	Rental rental = new Rental(movie, 5);
+    	Customer customer = new Customer("Mangesh");
+    	customer.addRental(rental);
+    	System.out.println(customer.statement());
+    	assertEquals( "Rental Record for Mangesh\n" + 
+    			"	ABCD	4.5\n" + 
+    			"Amount owed is 4.0\n" + 
+    			"You earned 1 frequent renter points", customer.statement());
+    }
+    
+    @Test
+    public void singleLoopVsMultiple() {
+      for (int j = 0; j < 100; j++) {
+
+        //single loop
+        int x = 0, y = 0, z = 0;
+        long l = System.currentTimeMillis();
+        for (int i = 0; i < 100000000; i++) {
+          x++;
+          y++;
+          z++;
+        }
+        l = System.currentTimeMillis() - l;
+
+
+        //multiple loops doing the same thing
+        int a = 0, b = 0, c = 0;
+        long m = System.currentTimeMillis();
+        for (int i = 0; i < 100000000; i++) {
+          a++;
+        }
+        for (int i = 0; i < 100000000; i++) {
+          b++;
+        }
+        for (int i = 0; i < 100000000; i++) {
+          c++;
+        }
+        m = System.currentTimeMillis() - m;
+        System.out.println(String.format("%d,%d", l, m));
+        
+      }
+    }
 }
